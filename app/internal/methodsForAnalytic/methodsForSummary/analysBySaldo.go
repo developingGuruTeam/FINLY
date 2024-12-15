d5dc195d996e878730aa099a61dbb3fc72c67bd3
maks <maks@MacBook-Air.local>
2024-12-15 23:25:09 +0300

    some fixes

---
+++ app/internal/methodsForAnalytic/methodsForSummary/analysBySaldo.go
@@ -0,0 +1,79 @@
+package methodsForSummary
+
+import (
+	"cachManagerApp/app/internal/methodsForAnalytic/methodsForExpenses"
+	"cachManagerApp/app/internal/methodsForAnalytic/methodsForIncomeAnalys"
+	"cachManagerApp/database"
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+// анализ сальдо за неделю
+func AnalyseBySaldoWeek(update tgbotapi.Update) (string, error) {
+
+	if database.DB == nil {
+		return "", fmt.Errorf("база данных не инициализирована")
+	}
+
+	analyticExpenses := methodsForExpenses.ExpensesHandler{DB: database.DB}
+	analyticIncomes := methodsForIncomeAnalys.AnalyticHandler{DB: database.DB}
+
+	totalWeekExpenses, err := analyticExpenses.ExpenseWeekAnalytic(update)
+	if err != nil {
+		return "", fmt.Errorf("ошибка при анализе расходов за неделю: %v", err)
+	}
+
+	totalWeekIncomes, err := analyticIncomes.IncomeWeekAnalytic(update)
+	if err != nil {
+		return "", fmt.Errorf("ошибка при анализе доходов за неделю: %v", err)
+	}
+
+	// Определяем категории с наибольшими расходами и доходами
+	var topExpenseCategory string
+	var maxExpense uint64
+
+	var topIncomeCategory string
+	var maxIncome uint64
+
+	// Суммируем расходы и находим топовую категорию
+	for category, amount := range totalWeekExpenses {
+		if amount > maxExpense {
+			maxExpense = amount
+			topExpenseCategory = category
+		}
+	}
+	// Суммируем доходы и находим топовую категорию
+	for category, amount := range totalWeekIncomes {
+		if amount > maxIncome {
+			maxIncome = amount
+			topIncomeCategory = category
+		}
+	}
+	// Генерация итогового текста
+	report := "📊 Ваш анализ за неделю по категориям:\n\n"
+
+	// Расходы по категориям
+	if len(totalWeekExpenses) > 0 {
+		report += "💸 Вы жадно тратили по категориям:\n"
+		for category := range totalWeekExpenses {
+			report += fmt.Sprintf("   ▪ %s\n", category)
+		}
+		report += fmt.Sprintf("\n😱 Больше всего расходов в категории: %s - %d\n", topExpenseCategory, maxExpense)
+	} else {
+		report += "💸 Расходов за неделю не обнаружено.\n"
+	}
+
+	report += "\n"
+
+	// Доходы по категориям
+	if len(totalWeekIncomes) > 0 {
+		report += "💵 Вы безжалостно зарабатывали по категориям:\n"
+		for category, _ := range totalWeekIncomes {
+			report += fmt.Sprintf("   ▪ %s\n", category)
+		}
+		report += fmt.Sprintf("\n🤑 Больше всего доходов в категории: %s - %d\n", topIncomeCategory, maxIncome)
+	} else {
+		report += "💵 Доходов за неделю не обнаружено.\n"
+	}
+	return report, nil
+}
